710ba0860154ac308c97c12c0709224528e70024
Silvia Parada <silviaparadag@yahoo.es>
2023-09-18 13:07:15 +0200

    Create starter kit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,11 @@
 import '../styles/layout/Header.scss';
 
 const Header = () => {
-    
-    return(
-        <header className="header">
-          <h1 className='header__title'>
-            Hola mundo!
-          </h1>
-          <h3 className='header__subtitle'>
-            Abrir consola
-          </h3>
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <h1 className="header__title">Ahoy!</h1>
+    </header>
+  );
+};
 
 export default Header;
